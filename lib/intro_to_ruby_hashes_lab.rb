c414f9bd4fd17b145d2fedb64605c73c018096ed
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-11 16:07:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,15 +3,15 @@ def new_hash{
 }
 end
 
-def my_hash{
+def my_hash
   # return a valid hash with any key/value pair of your choice
  
-}
+
 end
 
-def pioneer{
+def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-}
+
 end
 
 
